3a35fdedfa05574685f30bf942a1246e78cf9559
CodeTriumph-BasharJ <107142143+CodeTriumph-BasharJ@users.noreply.github.com>
2022-07-05 03:45:21 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.graphing;
+
+/**
+ *
+ * @author basha
+ */
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.stream.Stream;
+import java.util.Scanner;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)  {
+        // TODO code application logic here
+   
+    JFrame frame =new JFrame();
+    frame.add(new Graphing(file_open()));
+    frame.setSize(600,600);
+    frame.setLocation(400,400);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setVisible(true);
+  
+    
+    }
+    
+    private static List<String> file_open(){
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Input File Address: ");
+        String fileName = scan.nextLine();
+        Path path = Paths.get(fileName);
+        return read_file(path);
+        
+        
+    }
+    private static List<String> read_file(Path path){
+        List <String> Data = new ArrayList<>();
+        try(Stream <String> fileData = Files.lines(path)){
+            
+            fileData.forEach(e -> {
+                
+            Data.add(e);
+            
+            });
+            
+          
+        }catch(Exception m){
+            System.out.println(m.getMessage());
+            
+            
+        } 
+    return Data;
+    }
+}
+
